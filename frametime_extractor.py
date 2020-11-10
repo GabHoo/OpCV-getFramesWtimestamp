d7ef4761032b0e78c4e9d810c017f31668e86861
GabHoo <56197287+GabHoo@users.noreply.github.com>
2020-11-10 20:54:05 +0100

    Update frametime_extractor.py

--- frametime_extractor.py
+++ frametime_extractor.py
@@ -6,11 +6,12 @@ print('FPS= ' + str(video.get(cv2.CAP_PROP_FPS)) + '\n')
 
 while video.isOpened():
   	ret,frame = video.read()
-  	if ret == False : break
   	
-  	with open('times.txt', 'a') as the_file:
-    		the_file.write(str(int(video.get(cv2.CAP_PROP_POS_MSEC))) + ' ' + str(i).zfill(6) + '.png\n') #timestamp in millisec
-    		
-  	
+    if ret == False : break
+      
   	cv2.imwrite(str(i).zfill(6)+'.png',frame)
+  	
+    with open('times.txt', 'a') as the_file:
+    		the_file.write(str(int(video.get(cv2.CAP_PROP_POS_MSEC))) + ' ' + str(i).zfill(6) + '.png\n') #timestamp in millisec
+  	
   	i+=1
