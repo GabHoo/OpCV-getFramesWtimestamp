9068af3fce69f01eb776d071bf9b6b5c73ed0025
GabHoo <56197287+GabHoo@users.noreply.github.com>
2020-11-13 14:11:17 +0100

    Update frametime_extractor.py

--- frametime_extractor.py
+++ frametime_extractor.py
@@ -1,17 +1,18 @@
 import cv2
 
-video = cv2.VideoCapture('$INSERT NAME OR PATH.mp4')
+video = cv2.VideoCapture('RoomShoot_2_P20_frontcamera.mp4')
 i=000000
 print('FPS= ' + str(video.get(cv2.CAP_PROP_FPS)) + '\n')
 
 while video.isOpened():
   	ret,frame = video.read()
+  	if ret == False : break
   	
-    if ret == False : break
-      
-  	cv2.imwrite(str(i).zfill(6)+'.png',frame)
+  	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
   	
-    with open('times.txt', 'a') as the_file:
-    		the_file.write(str(int(video.get(cv2.CAP_PROP_POS_MSEC))) + ' ' + str(i).zfill(6) + '.png\n') #timestamp in millisec
+  	cv2.imwrite(str(i).zfill(6)+'.png',gray)
   	
+  	with open('times.txt', 'a') as the_file: #nome del file timestamp
+    		the_file.write(str(int(video.get(cv2.CAP_PROP_POS_MSEC))/1000) + ' ' + str(i).zfill(6) + '.png\n') #timestimap in seconds
+    		#the_file.write(str(i).zfill(6) +'\n') #just stamps
   	i+=1
